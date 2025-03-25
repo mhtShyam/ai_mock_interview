0f78c0ccd82f0a3e3eacce0838a1663da761eaeb
shyam <shyammht3@gmail.com>
2025-03-25 23:08:43 +0530

    login logup with firebase

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,15 +3,16 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyADz3yksma3ccQh5hO0LEjJVADAMXpWcVU",
-  authDomain: "prepwise-e0db9.firebaseapp.com",
-  projectId: "prepwise-e0db9",
-  storageBucket: "prepwise-e0db9.firebasestorage.app",
-  messagingSenderId: "421563257399",
-  appId: "1:421563257399:web:0b343324e016f4fdff65b2",
-  measurementId: "G-1JKGBHNCPR"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
