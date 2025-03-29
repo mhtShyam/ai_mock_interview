92e57fc9a14ba8ec36da094431a18123ba6c07a0
shyam <shyammht3@gmail.com>
2025-03-29 23:26:13 +0530

    implementation of interview session

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import {useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { vapi } from "@/lib/vapi.sdk";
+import { interviewer } from "@/constants";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -17,7 +18,7 @@ interface SaveMessage{
   content:string;
 }
 
-const Agent = ({ userName, userId, type }: AgentProps) => {
+const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) => {
   const router = useRouter();
   const [isSpeaking, setIsSpeaking] = useState(false)
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
@@ -55,18 +56,52 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
     }
   },[])
 
+  const handleGenerateFeedback= async (messages:SaveMessage[])=>{
+      console.log("generated feed back here", messages)
+      const {success, id}={
+        success:true,
+        id:'feedback-id'
+      }
+
+      if(success && id){
+        router.push(`/interview/${interviewId}/feedback`)
+      }else{
+        console.log("error saving feedback")
+        router.push('/')
+      }
+  }
+
   useEffect(()=>{
-    if(callStatus === CallStatus.FINISHED) router.push('/')
+    if(callStatus === CallStatus.FINISHED){
+      if(type ==="generate"){
+        router.push('/')
+      }else{
+        handleGenerateFeedback(messages);
+      }
+    }
+  
   },[messages, callStatus, type, userId])
 
 const handleCall = async()=>{
   setCallStatus(CallStatus.CONNECTING);
-  await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,{
-    variableValues:{
-      username: userName,
-      userid: userId
+  if(type === 'generate'){
+    await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,{
+      variableValues:{
+        username: userName,
+        userid: userId
+      }
+    });
+  }else{
+    let formattedQuestions = "";
+    if(questions){
+      formattedQuestions = questions.map((question)=> ` - ${question}`).join('\n');
+      await vapi.start(interviewer,{
+        variableValues:{
+          questions:formattedQuestions
+        }
+      })
     }
-  });
+  }
 }
 const handleDisconnect = async()=>{
   setCallStatus(CallStatus.FINISHED);
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechicons from './DisplayTechicons';
 
-const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
+const InterviewCard = ({id, userId, role, type, techstack, createdAt}:InterviewCardProps) => {
   const feedback = null as Feedback | null;
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
   const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMM D, YYYY")
@@ -36,7 +36,7 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}:I
         <div className='flex flex-row justify-between'>
           <DisplayTechicons techStack={techstack}/>
           <Button className='btn-primary'>
-            <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>{feedback ? "Check Feedback": "View Interview"}</Link>
+            <Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}>{feedback ? "Check Feedback": "View Interview"}</Link>
           </Button>
         </div>
       </div>
